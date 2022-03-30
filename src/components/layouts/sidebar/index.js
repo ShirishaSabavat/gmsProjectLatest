e4e9b13ad83bfcdebed535332d205f7651db292f
nishant <nishant@atraphygital.com>
2022-03-30 14:54:01 +0530

    Added header and sidebar

--- src/components/layouts/sidebar/index.js
+++ src/components/layouts/sidebar/index.js
@@ -1,29 +1,9 @@
 import { Menu } from 'antd';
-import {
-  HomeOutlined,
-} from '@ant-design/icons';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { history } from 'redux/store';
 
-const { SubMenu, Item } = Menu;
-
-const sidebarMenu = [
-  {
-    title: 'Home',
-    icon: HomeOutlined,
-    subMenu: [
-      {
-        key: '/home/dashboard',
-        label: 'Dashboard',
-      },
-      {
-        key: '/partner/partner',
-        label: 'Partner',
-      },
-    ],
-  },
-];
+const { Item } = Menu;
 
 const Sidebar = ({
   location: {
@@ -31,74 +11,81 @@ const Sidebar = ({
   },
   sideBarLayout,
   permission,
-}) => (
-  <>
-    <div className={`py-3 px-6 flex items-center border-b border-gray-100 ${sideBarLayout ? 'justify-center' : ''}`}>
-      <img
-        src="/assets/images/logo/app-logo.svg"
-        alt="app-logo"
-        className="h-8"
-      />
-    </div>
-    <Menu
-      style={{ borderRight: 'none' }}
-      selectedKeys={[pathname]}
-      onClick={({ key }) => history.push(key)}
-      mode="inline"
-    >
-      {sidebarMenu.map(({
-        title,
-        subMenu,
-        icon: Icon,
-        key: menuKey,
-        permission: mainPermission,
-      }) => {
-        // sub menu
-        if (subMenu && subMenu.length) {
-          return (
-            <SubMenu
-              key={title}
-              icon={<Icon style={{ paddingBottom: '0.22rem' }} />}
-              className="text-xs"
-              title={title}
-            >
-              {subMenu && subMenu.map(({
-                key,
-                label,
-                permission: subPermission,
-              }) => {
-                // permission wise rendering for sub menu
-                if (!permission.includes(subPermission)
-                  && subPermission) return null;
-                return (
-                  <Item
-                    key={key}
-                    className="text-xs"
-                  >
-                    {label}
-                  </Item>
-                );
-              })}
-            </SubMenu>
-          );
-        }
-        // main menu
-        // permission wise rendering for sub menu
-        if (!permission.includes(mainPermission)
+}) => {
+  const sidebarMenu = [
+    {
+      title: 'Home',
+      icon: '/assets/images/logo/sidebarLogo/home.svg',
+      key: '/home/dashboard',
+    },
+    {
+      title: 'Cities',
+      icon: '/assets/images/logo/sidebarLogo/cities.svg',
+      key: '/partner/partner',
+    },
+    {
+      title: 'Garage',
+      icon: '/assets/images/logo/sidebarLogo/garage.svg',
+      key: '/home/garage',
+    },
+    {
+      title: 'People',
+      icon: '/assets/images/logo/sidebarLogo/people.svg',
+      key: '/partner/people',
+    },
+  ];
+
+  // const settingPath = '/setting';
+  return (
+    <>
+      <div className={`py-3 px-6 flex items-center border-b border-gray-100 ${sideBarLayout ? 'justify-center' : ''}`}>
+        <img
+          src="/assets/images/logo/app-logo.svg"
+          alt="app-logo"
+          className="h-8"
+        />
+      </div>
+      <Menu
+        style={{ borderRight: 'none' }}
+        mode="inline"
+      >
+        {sidebarMenu.map(({
+          title,
+          icon,
+          key: menuKey,
+          permission: mainPermission,
+        }) => {
+          // main menu
+          // permission wise rendering for sub menu
+          if (!permission.includes(mainPermission)
           && mainPermission) return null;
-        return (
-          <Item
-            icon={<Icon style={{ paddingBottom: '0.22rem' }} />}
-            key={menuKey}
-            className="text-xs"
-          >
-            {title}
-          </Item>
-        );
-      })}
-    </Menu>
-  </>
-);
+          return (
+            <Item
+              icon={<img style={{ paddingBottom: '0.22rem' }} src={icon} alt={icon} />}
+              key={menuKey}
+              className="text-xs"
+            >
+              {title}
+            </Item>
+          );
+        })}
+      </Menu>
+      <Menu
+        style={{ borderRight: 'none', marginTop: '95%' }}
+        mode="inline"
+        selectedKeys={[pathname]}
+        onClick={({ key }) => history.push(key)}
+      >
+        <Item
+          icon={<img style={{ paddingBottom: '0.22rem' }} src="/assets/images/logo/sidebarLogo/settings.svg" alt="settings" />}
+          className="text-xs"
+        >
+          Settings
+        </Item>
+      </Menu>
+    </>
+  );
+};
 
 const mapStateToProps = ({ userReducer }) => ({
   permission: userReducer.permission,
--- src/layout/main/index.js
+++ src/layout/main/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Layout } from 'antd';
 import AppHeader from 'components/layouts/AppHeader';
 import Sidebar from 'components/layouts/sidebar';
@@ -12,15 +12,15 @@ const {
 const MainLayout = ({
   children,
 }) => {
-  const [sideBarLayout, setSideBarLayout] = useState(false);
+  // const [sideBarLayout, setSideBarLayout] = useState(false);
 
   useEffect(() => {
     let timeout = null;
     const onHandleResize = () => {
       if (timeout) clearTimeout(timeout);
       timeout = setTimeout(() => {
-        if (window.innerWidth < 1580) setSideBarLayout(true);
-        if (window.innerWidth > 1580) setSideBarLayout(false);
+        // if (window.innerWidth < 1580) setSideBarLayout(true);
+        // if (window.innerWidth > 1580) setSideBarLayout(false);
       }, 150);
     };
 
@@ -38,17 +38,8 @@ const MainLayout = ({
       </Header>
       <Layout>
         <Sider
-          collapsible
-          collapsed={sideBarLayout}
           width={270}
-          defaultCollapsed={false}
           className="h-screen sticky top-0 font-mulish-semi-bold text-xs border-r-4 border-gray-100"
-          trigger={(
-            <div className="border-t border-r-4 border-gray-100">
-              {sideBarLayout ? 'Expand' : 'Collapse'}
-            </div>
-          )}
-          onCollapse={() => setSideBarLayout((prevState) => !prevState)}
         >
           <Sidebar />
         </Sider>
